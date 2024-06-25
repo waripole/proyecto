1abb3e3bc63f6232bb783138befb6c8ac209b825
waripole <iliketocookireallydo@gmail.com>
2024-06-24 21:25:02 -0400

    Add files via upload

--- simulador.py
+++ simulador.py
@@ -1,1 +1,25 @@
-Simulador
+from comunidad import Comunidad
+
+#hacer el marca pasos
+
+class Simulador:
+	#Atributos
+	def __init__(self, comunidad):
+		self._comunidad = comunidad
+	
+
+#-----------------------------------------------------------
+	#Getters
+	def get_comunidad(self):
+		return self._comunidad
+
+#-----------------------------------------------------------
+	#Setters
+	def set_comunidad(self, comunidad):
+		self._comunidad = comunidad
+
+#-----------------------------------------------------------
+	#Mètodos
+
+	def correr(self):
+		pass
