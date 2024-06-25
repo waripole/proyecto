9c70614ba0b86915e9b5609d43d938c1db405e12
waripole <iliketocookireallydo@gmail.com>
2024-06-24 22:49:42 -0400

    test

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from simulador import Simulador
 
 covid = Enfermedad( infeccion_probable = 0.3 , promedio_pasos = 18, enfermo = False, contador = 0)
 
-talca = Comunidad( num_ciudadanos = 1000, #error aber
+talca = Comunidad( num_cuidadano = 1000, #error aber - estaba mal escroto
 					promedio_conexion_fisica = 8,
 					enfermedad = "covid",
 					num_infectados = 10,
 					probabilidad_conexion_fisica = 0.8,
-					archivo_cvs = "lista.csv")
+					archivo_csv = "lista.csv")
 
+#probando para ver si se crean correctamente - sim
+talca.imprimir_cuidadanos()
 
-sim = Simulador()
+sim = Simulador(talca)
 sim.set_comunidad(comunidad = talca )
-sim.run(pasos = 45)
+#sim.run(pasos = 45)
